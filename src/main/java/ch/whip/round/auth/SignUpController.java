044065942f54dd993bb198a5128a96b25d21a395
Anton Dmitrijev <anton@tripledev.ee>
2016-03-05 12:55:13 +0100

    add rest operations stub

--- src/main/java/ch/whip/round/auth/SignUpController.java
+++ src/main/java/ch/whip/round/auth/SignUpController.java
@@ -1,8 +1,22 @@
 package ch.whip.round.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestOperations;
 
 @RestController
 public class SignUpController {
+    @Value("${paymit.endpoint}")
+    private String paymitEndpoint;
+    @Autowired
+    private RestOperations restOperations;
 
+    @RequestMapping(path = "/signup/{phoneNumber}", method = RequestMethod.GET)
+    public Integer signUpWithPhoneNumber(@PathVariable String phoneNumber) {
+        return 1111;
+    }
 }
