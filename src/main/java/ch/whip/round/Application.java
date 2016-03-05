ba25256d44c36b380c6430b920e8cfc8af18bb17
Anton Dmitrijev <anton@tripledev.ee>
2016-03-05 14:27:35 +0100

    produce a deployable war file

--- src/main/java/ch/whip/round/Application.java
+++ src/main/java/ch/whip/round/Application.java
@@ -2,11 +2,19 @@ package ch.whip.round;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @ComponentScan
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
